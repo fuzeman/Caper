2218587086e01ecc1e30705045f29012ec05c491
Dean Gardiner <gardiner91@gmail.com>
2013-09-08 01:43:18 +1200

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='caper',
+    version='0.1',
+    packages=['caper', 'caper.fragment_parsers'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Dean Gardiner',
+    author_email='gardiner91@gmail.com',
+    description=''
+)
